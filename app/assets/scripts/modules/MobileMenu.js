a6397577e3f7c912d874ac10dd164e0a48d750a2
Anton Horn <anton.h@posteo.de>
2018-09-29 01:11:06 +0200

    finished lesson 45

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -2,6 +2,7 @@ import $ from "jquery";
 
 class MobileMenu{
     constructor(){
+        this.siteHeader = $(".site-header");
         this.menuIcon = $(".site-header__menu-icon");
         this.menuContent = $(".site-header__menu-content");
 
@@ -13,7 +14,9 @@ class MobileMenu{
     }
 
     toggleMenu(){
+        this.siteHeader.toggleClass("site-header--is-expanded");
         this.menuContent.toggleClass("site-header__menu-content--is-visible");
+        this.menuIcon.toggleClass("site-header__menu-icon--close-x");
     }
 }
 
